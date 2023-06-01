f7f6a6765b72d81e8e8fc03887c5b9a9374a753c
Tamirys Nogueira <tamirysnogueira22@hotmail.com>
2023-06-01 17:46:10 -0300

    stock correction

--- mongo/listaProdutosComBomEstoque.js
+++ mongo/listaProdutosComBomEstoque.js
@@ -1,8 +1,8 @@
 use('ecomm')
 
 const nameAndStock = db.products.find(
-    {"quantidade": {$gte: 3}}, 
-    {_id: 1, "nome": 1, "quantidade": 1}
+    {"estoque": {$gte: 3}}, 
+    {_id: 1, "nome": 1, "estoque": 1}
 )
 
 console.log(nameAndStock)
