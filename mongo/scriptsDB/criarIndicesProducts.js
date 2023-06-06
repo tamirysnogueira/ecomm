0b42ac897e17b32e65490f6f251ea410b2946c01
Tamirys Nogueira <tamirysnogueira22@hotmail.com>
2023-06-06 11:46:10 -0300

    creating the scriptsDB folder for the code to be organized

---
+++ mongo/scriptsDB/criarIndicesProducts.js
@@ -0,0 +1,9 @@
+use("ecomm");
+
+const indexOfCategoria = db.products.createIndex({ categoria: 1 })
+const indexOfNome = db.products.createIndex({ nome: 1 })
+const indexOfPreco = db.products.createIndex({ preco: 1 })
+
+console.log(indexOfCategoria)
+console.log(indexOfNome)
+console.log(indexOfPreco)
