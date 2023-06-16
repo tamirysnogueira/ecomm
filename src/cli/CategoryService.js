cc0d1dc02ab016fa482eadeca87bb9fd264fc45a
Tamirys Nogueira <tamirysnogueira22@hotmail.com>
2023-06-16 12:30:00 -0300

    refactoring the code

--- src/cli/CategoryService.js
+++ src/cli/CategoryService.js
@@ -1,113 +1,78 @@
-import chalk from "chalk";
-import fs from 'fs'
+import fs from 'fs';
+
 export default class CategoryService {
-    static async findCategories() {
-        const listOProductsFromDatabase = await fetch('http://localhost:3000/categories')
-        const listOProducts = await listOProductsFromDatabase.json();
-        const status = listOProductsFromDatabase.status
+  static async getDatasOfAPI(path, options = {}) {
+    try {
+      const responseOfDatabase = await fetch(`http://localhost:3000/${path}`, options);
+      const dataOfDatabase = await responseOfDatabase.json();
+      const { status, statusText } = responseOfDatabase;
 
-        console.log(chalk.black.bgGray(`Response Status: ${status}`))
+      const message = `Response Status: ${status} \n ${statusText}`;
 
-        return listOProducts
+      const datas = {
+        dataOfDatabase,
+        responseStatus: status,
+        responseStatusText: statusText,
+        message,
+      };
 
+      return datas;
+    } catch (error) {
+      throw new Error(error);
     }
+  }
 
-    static async findCategoryById(idRequired) {
-        const listOProductsFromDatabase = await fetch('http://localhost:3000/categories')
-        const listOProducts = await listOProductsFromDatabase.json();
-        const status = listOProductsFromDatabase.status
+  static async findCategories() {
+    const datasCategories = await this.getDatasOfAPI('categories');
 
-        const productFoundById = listOProducts.find( product => { 
-            if(product.id === idRequired) {return product}
+    return { ...datasCategories };
+  }
 
-        })
+  static async findCategoryById(idRequired) {
+    const datasCategorieById = await this.getDatasOfAPI(`categories/${idRequired}`);
 
-        if(productFoundById != undefined) {
-            console.log(chalk.black.bgGreen(`Response Status: ${status}`))
-            return productFoundById
-        } 
+    return { ...datasCategorieById };
+  }
 
-        if(productFoundById === undefined) {
-            const messageError = chalk.black.bgRed(`Response Status: 404. Id não encontrado`)
-            return messageError
-        }
-    }
+  static async createCategory(pathOfProductJson) {
+    const productJson = await fs.promises.readFile(pathOfProductJson, 'utf-8');
 
-    static async createCategory (pathOfProductJson) {
-        const productJson = await fs.promises.readFile(pathOfProductJson, 'utf-8')
+    const optionsFetch = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: productJson,
+    };
 
-        const options = {
-            method: 'POST',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: productJson
-        }
+    const datasCreatedCategorie = await this.getDatasOfAPI('categories', optionsFetch);
 
-        try {
-            const insertResponse = await fetch('http://localhost:3000/categories', options)
-            const response = await insertResponse.json()
-            const status = insertResponse.status
-            const statusText = insertResponse.statusText
+    return { ...datasCreatedCategorie };
+  }
 
-            console.log(response)
+  static async updateCategory(idOfCategory, pathOfCategoryJson) {
+    const categoryToBeUpdate = await fs.promises.readFile(pathOfCategoryJson, 'utf-8');
 
-            const messageSuccess = chalk.black.bgGreen(`Response Status: ${status} \n ${statusText}`)
+    const optionsFetch = {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: categoryToBeUpdate,
+    };
 
-            return messageSuccess
-        } catch (error) {
-            return error
-        }
+    const datasUpdatedCategorie = await this.getDatasOfAPI(`categories/${idOfCategory}`, optionsFetch);
 
-    }
+    return { ...datasUpdatedCategorie };
+  }
 
-    static async updateCategory  (idOfCategory, pathOfCategoryJson ) {
-        const categoryToBeUpdate = await fs.promises.readFile(pathOfCategoryJson, 'utf-8')
+  static async deleteCategory(idOfCategory) {
+    const optionsFetch = {
+      method: 'DELETE',
+    };
 
-        const optionsFetch = {
-            method: 'PUT',
-            headers: {
-              'Content-Type': 'application/json'
-            },
-            body: categoryToBeUpdate
-        }
+    const deletedCategory = await this.getDatasOfAPI(`categories/${idOfCategory}`, optionsFetch);
 
-        try {
-            const updatedCategoryResponse = await fetch(`http://localhost:3000/categories/${idOfCategory}`, optionsFetch)
-            const updatedCategoryJson = await updatedCategoryResponse.json()
-            const statusOfResponse = updatedCategoryResponse.status
-            const statusTextOfResponse = updatedCategoryResponse.statusText
-
-            console.log(updatedCategoryJson)
-
-            const messageSuccess = chalk.black.bgGreen(`Response Status: ${statusOfResponse} \n ${statusTextOfResponse}`)
-
-            return messageSuccess
-        } catch (error) {
-            return error
-        }
-    }
-
-    static async deleteCategory  (idOfCategory ) {
-        const optionsFetch = {
-            method: 'DELETE'
-        }
-
-        try {
-            const deletedCategoryResponse = await fetch(`http://localhost:3000/categories/${idOfCategory}`, optionsFetch)
-            const deletedCategoryJson = await deletedCategoryResponse.json()
-            const statusOfResponse = deletedCategoryResponse.status
-            const statusTextOfResponse = deletedCategoryResponse.statusText
-
-            console.log(deletedCategoryJson)
-
-            const messageSuccess = chalk.black.bgGreen(`Response Status: ${statusOfResponse} \n ${statusTextOfResponse}`)
-
-            return messageSuccess
-        } catch (error) {
-            return error
-        }
-    }
-
-
+    return { ...deletedCategory };
+  }
 }
--- src/cli/cli.js
+++ src/cli/cli.js
@@ -1,59 +1,77 @@
-import fs from 'fs'
+/* eslint-disable import/extensions */
+/* eslint-disable no-case-declarations */
+import chalk from 'chalk';
 import CategoryService from './CategoryService.js';
 
-const consolePath = process.argv
+const consolePath = process.argv;
 
-async function processarComando(args) {
-    const databasePath = args[2]
+function printContent(content) {
+  const {
+    dataOfDatabase,
+    responseStatus,
+    message,
+  } = content;
 
-    if(databasePath === undefined) { 
-        return console.log(new Error('O caminho do arquivo/diretório é obrigatório')) 
-    }
+  console.log(dataOfDatabase);
 
-    switch (databasePath) {
-        case '--listarCategorias':
-            const listOProducts = await CategoryService.findCategories()
-            console.log(listOProducts)
+  if (responseStatus >= 400) {
+    return console.log(chalk.black.bgRed(message));
+  }
 
-            break;
-        
-        case '--recuperarCategoriaPorId':
-            const idOfCategory = Number(args[3])
-
-            const categoryById = await CategoryService.findCategoryById(idOfCategory)
-
-            console.log(categoryById)
-            break;
-
-        case '--inserirCategoria':
-            const pathOfCategoryJson = args[3]
-    
-            const insertedCategory = await CategoryService.createCategory(pathOfCategoryJson)
-    
-            console.log(insertedCategory)
-
-            break;
-        case '--atualizarCategoria':
-            const categoryIdToBeUpdated = args[3]
-            const pathOfCategoryToBeUpdated = args[4]
-        
-            const updatedCategory = await CategoryService.updateCategory(categoryIdToBeUpdated, pathOfCategoryToBeUpdated)
-        
-            console.log(updatedCategory)
-    
-            break;
-        case '--excluirCategoria':
-            const categoryIdToBeDeleted = args[3]
-            
-            const deletedCategory = await CategoryService.deleteCategory(categoryIdToBeDeleted)
-            
-            console.log(deletedCategory)
-        
-            break;
-        default:
-            console.log('Não há esse caminho disponível')
-            break;
-    }
+  return console.log(chalk.black.bgGreen(message));
 }
 
-processarComando(consolePath)
+// eslint-disable-next-line consistent-return
+async function processarComando(args) {
+  const databasePath = args[2];
+
+  if (databasePath === undefined) {
+    return console.log(new Error('O caminho do arquivo/diretório é obrigatório'));
+  }
+
+  switch (databasePath) {
+    case '--listarCategorias':
+      const categories = await CategoryService.findCategories();
+      printContent(categories);
+
+      break;
+
+    case '--recuperarCategoriaPorId':
+      const idOfCategory = Number(args[3]);
+
+      const categoryById = await CategoryService.findCategoryById(idOfCategory);
+      printContent(categoryById);
+
+      break;
+
+    case '--inserirCategoria':
+      const pathOfCategoryJson = args[3];
+
+      const insertedCategory = await CategoryService.createCategory(pathOfCategoryJson);
+
+      printContent(insertedCategory);
+
+      break;
+    case '--atualizarCategoria':
+      const categoryIdToBeUpdated = args[3];
+      const pathOfCategoryToBeUpdated = args[4];
+
+      const updatedCategory = await CategoryService.updateCategory(categoryIdToBeUpdated, pathOfCategoryToBeUpdated);
+
+      printContent(updatedCategory);
+
+      break;
+    case '--excluirCategoria':
+      const categoryIdToBeDeleted = args[3];
+
+      const deletedCategory = await CategoryService.deleteCategory(categoryIdToBeDeleted);
+
+      printContent(deletedCategory);
+
+      break;
+    default:
+      throw new Error('Não há esse caminho disponível');
+  }
+}
+
+processarComando(consolePath);
